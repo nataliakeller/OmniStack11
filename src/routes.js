47907f9c7510eb3bcbcb6bf348ca629a2286e5d8
nataliakeller <natalia-farah@uergs.edu.br>
2020-03-30 23:50:43 +0200

    <Setting the connection, and starting the database>

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,25 @@ const connection = require('./database/connection') // Fazendo a conexão com o 
 
 const routes = express.Router() // Retirando o módulo de rotas em uma nova variável
 
-routes.post('/ongs', (request, response) => {      //Criando a primeira rota da aplicação
+routes.post('/ongs', async (request, response) => {      //Criando a primeira rota da aplicação
     const { name, email, whatsapp, city, uf } = request.body; // Preenchimento do cadastro
 
     const id = crypto.randomBytes(4).toString('HEX'); // Gerando o ID para a ONG usando o crypto
     
-    return response.json();
+    /*
+    * Explicação sobre o await: como o insert pode demorar para ser finalizado, é necessário 
+    * retornar o resultado dessa ong (que foi recem criada) só depois que esse insert foi finalizado. 
+    * Por isso fazemos uso do async/await
+    */
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+    return response.json({ id });
         
 });
 
