0573c96397d80d4d13ba6b347b1f784d21c78aac
nataliakeller <natalia-farah@uergs.edu.br>
2020-03-30 02:01:51 +0200

    < Installed: Nodemon, Express >

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # omnistack11
-OmniStack 11ª semana 
+--- OmniStack 11ª semana ---
+
+
+Creating a site with nodejs and react instructed by Rocketseat one-week-bootcamp.
---
+++ index.js
@@ -0,0 +1,21 @@
+//Importando as funcionalidades do express
+
+const express = require('express'); // Importando o módulo express pra dentro da variável express
+
+const app = express(); // Variável para armazenar a aplicação. Criando a aplicação
+
+app.use(express.json()); // Informando ao app/express, que estaremos utilizando json para o corpo das requisições
+
+app.listen(3333); // Aplicação vai ser rodada na porta 3333 (localhost:3333)
+
+app.post('/users', (request, response) => {      //Criando a primeira rota da aplicação
+
+    const body = request.body;
+    
+
+    return response.json({
+        evento: 'Omnistack',
+        aluno: 'Natália Keller'
+    });
+        
+});
