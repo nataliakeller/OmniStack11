97bb33bb3106bb9d119a8cb9eef271c09ef3a976
nataliakeller <natalia-farah@uergs.edu.br>
2020-04-02 20:25:39 +0200

    <Setting the session/login>

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const express = require('express'); // Importando o express
 
 const OngController = require('./controllers/OngController'); // Importando OngController
 const IncidentController = require('./controllers/IncidentController'); // Importando OngController
+const ProfileController = require('./controllers/ProfileController'); // Importando ProfileController
+const SessionController = require('./controllers/SessionController'); // Importando SessionController
 
 const connection = require('./database/connection'); // Fazendo a conexão com o banco de dados
 
@@ -15,5 +17,9 @@ routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes; // exportando as rotas
