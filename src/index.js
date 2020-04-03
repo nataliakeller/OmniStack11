8583a7b8723f30689c3703c4efaf75649cf61f5c
nataliakeller <natalia-farah@uergs.edu.br>
2020-04-03 21:35:12 +0200

    minor edit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 const express = require('express'); // Importando o módulo express pra dentro da variável express
 const cors = require('cors');
 const routes = require('./routes');
-//testando
+
 const app = express(); // Variável para armazenar a aplicação. Criando a aplicação
 
 app.use(cors());
