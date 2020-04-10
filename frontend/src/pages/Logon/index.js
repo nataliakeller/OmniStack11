508c456b6d3a5e409f8799ddcc29fc08abb5ca0c
nataliakeller <natalia-farah@uergs.edu.br>
2020-04-10 23:49:27 +0200

    <setting logon, profile, connecting with the db>

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,10 @@
 // Link from react-router-dom serve para, finalmente, por em prática principal utilidade de uma SPA
 // Com isso, a página não recarrega inteira e só muda a rota. 
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import { FiLogIn } from 'react-icons/fi';
 
@@ -10,13 +12,30 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('/sessions', { id });
+            
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+        } catch (err) {
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
             <img src={logoImg} alt=""/>
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça logon</h1>
-                    <input placeholder="Sua Id" />
+                    <input placeholder="Sua Id" value={id} onChange={e => setId(e.target.value)} />
                     <button type="submit" className="button">Entrar</button>
 
                     <Link className="back-link" to="/register"> 
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -5,11 +5,12 @@ import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const ongName = localStorage.getItem('ongName');
     return (
     <div className="profile-container">
         <header>
             <img src={ logoImg } alt="Be The Hero"/>
-            <span>Bem-vinda, APAD!</span>
+            <span>Bem-vinda, {ongName}!</span>
 
             <Link className="button" to="/incidents/new"> Cadastrar novo caso</Link>
             <button>
