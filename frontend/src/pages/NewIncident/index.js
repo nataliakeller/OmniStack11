3439f1e8906c2580960eea27f6772b02af926089
nataliakeller <natalia-farah@uergs.edu.br>
2020-04-11 22:41:23 +0200

    <NewIncident connected with the db, front end done>

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import api from '../../services/api';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft } from 'react-icons/fi';
@@ -7,6 +8,36 @@ import { FiArrowLeft } from 'react-icons/fi';
 
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+        const data = {
+            title,
+            description,
+            value,
+        };
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+
+            });
+            alert('Cadastro do caso foi efetuado com sucesso!');
+            history.push('/profile');
+        } catch (err) {
+            alert('Erro ao cadastrar caso, tente novamente.');
+            
+        }
+        
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -20,10 +51,22 @@ export default function NewIncident() {
                         Voltar para o início
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Título do caso"/>
-                    <textarea placeholder="Descrição"></textarea>
-                    <input placeholder="Valor em R$"/>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                    placeholder="Título do caso"
+                    value={title}
+                    onChange={ e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                    placeholder="Descrição" 
+                    value={description}
+                    onChange={ e => setDescription(e.target.value)}
+                    />
+                    <input 
+                    placeholder="Valor em R$"
+                    value={value}
+                    onChange={ e => setValue(e.target.value)}
+                    />
                     
 
                     <button type="submit" className="button">Cadastrar</button>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
@@ -7,9 +7,12 @@ import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
 export default function Profile() {
+    const history = useHistory();
+
     const [ incidents, setIncidents ] = useState([]);
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+    
 
     useEffect(() => {
         api.get('profile', {
@@ -36,6 +39,11 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
     <div className="profile-container">
         <header>
@@ -43,7 +51,7 @@ export default function Profile() {
             <span>Bem-vinda, {ongName}!</span>
 
             <Link className="button" to="/incidents/new"> Cadastrar novo caso</Link>
-            <button>
+            <button type="button" onClick={handleLogout}>
                 <FiPower size={18} color="#E02041" />
             </button>
         </header>
